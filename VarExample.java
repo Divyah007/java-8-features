4d6593d04f81d5e1e181f0a24f4059728f2065f0
Divya Lakshmi V <divyahashokkumar@gmail.com>
2023-06-11 18:05:20 +0530

    added var feature

---
+++ VarExample.java
@@ -0,0 +1,17 @@
+
+//var cannot be used as instance/class variable
+//var can be used for int,boolean.double,..
+//var<?> it can be used in collections
+//var was introduced in java 10
+
+
+public class VarExample {
+    public static void main(String[] args) {
+var g=889;
+        System.out.println(g);
+}
+
+    }
+
+
+
